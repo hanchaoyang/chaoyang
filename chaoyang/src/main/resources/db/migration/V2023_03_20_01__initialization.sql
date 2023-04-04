1d970680127fe8d7fbe00ded7663e9991635cf8a
韩朝阳 <468455321@qq.com>
2023-04-04 17:01:47 +0800

    feat: 修改初始化SQL

--- chaoyang/src/main/resources/db/migration/V2023_03_20_01__initialization.sql
+++ chaoyang/src/main/resources/db/migration/V2023_03_20_01__initialization.sql
@@ -35,3 +35,20 @@ CREATE TABLE `permission` (
     `name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO `chaoyang`.`user`(`id`, `nickname`, `password`, `phone`, `status`) VALUES (1, '朝阳', '123456', '18888888888', 1);
+
+INSERT INTO `chaoyang`.`role`(`id`, `code`, `name`) VALUES (1, 'super', '超级管理员');
+INSERT INTO `chaoyang`.`role`(`id`, `code`, `name`) VALUES (2, 'admin', '管理员');
+
+INSERT INTO `chaoyang`.`permission`(`id`, `code`, `name`) VALUES (1, 'user:find', '查询用户');
+INSERT INTO `chaoyang`.`permission`(`id`, `code`, `name`) VALUES (2, 'user:create', '添加用户');
+INSERT INTO `chaoyang`.`permission`(`id`, `code`, `name`) VALUES (3, 'user:modify', '修改用户');
+INSERT INTO `chaoyang`.`permission`(`id`, `code`, `name`) VALUES (4, 'user:remove', '删除用户');
+
+INSERT INTO `chaoyang`.`user_role`(`role_id`, `user_id`) VALUES (1, 1);
+
+INSERT INTO `chaoyang`.`role_permission`(`permission_id`, `role_id`) VALUES (1, 1);
+INSERT INTO `chaoyang`.`role_permission`(`permission_id`, `role_id`) VALUES (2, 1);
+INSERT INTO `chaoyang`.`role_permission`(`permission_id`, `role_id`) VALUES (3, 1);
+INSERT INTO `chaoyang`.`role_permission`(`permission_id`, `role_id`) VALUES (4, 1);
