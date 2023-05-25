003ed6fe8d732e3b6883b1d2466fcd9f2f3907e7
韩朝阳 <468455321@qq.com>
2023-05-25 17:04:58 +0800

    feat: 数据校验异常处理

--- chaoyang/src/main/java/com/chaoyang/example/exception/GlobalExceptionHandler.java
+++ chaoyang/src/main/java/com/chaoyang/example/exception/GlobalExceptionHandler.java
@@ -1,10 +1,15 @@
 package com.chaoyang.example.exception;
 
+import cn.hutool.core.collection.CollectionUtil;
 import com.chaoyang.example.entity.dto.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindException;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 /**
  * 全局异常处理器
@@ -45,6 +50,22 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public Result<Void> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return Result.of(400, "参数错误");
+    }
+
+    /**
+     * 参数校验异常
+     */
+    @ExceptionHandler(BindException.class)
+    public Result<Void> handleBindException(BindException e) {
+        List<ObjectError> errors = e.getAllErrors();
+
+        if (CollectionUtil.isNotEmpty(errors)) {
+            ObjectError error = errors.get(0);
+
+            return Result.of(400, error.getDefaultMessage());
+        }
+
         return Result.of(400, "参数错误");
     }
 
