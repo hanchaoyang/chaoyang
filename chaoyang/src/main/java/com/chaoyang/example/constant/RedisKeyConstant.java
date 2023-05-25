14cdb62a50a4d50a5720c02f4a8a8a8fdd366b6f
韩朝阳 <468455321@qq.com>
2023-05-25 17:05:27 +0800

    feat: Redis键常量类

--- chaoyang/src/main/java/com/chaoyang/example/constant/RedisKeyConstant.java
+++ chaoyang/src/main/java/com/chaoyang/example/constant/RedisKeyConstant.java
@@ -8,6 +8,40 @@ package com.chaoyang.example.constant;
  */
 public class RedisKeyConstant {
 
-    public static final String LOGIN_INFO = "chaoyang:login-info:%s";
+    /**
+     * 登录信息
+     */
+    @Deprecated
+    public static final String LOGIN_INFO = "login-info:%s";
+
+    /**
+     * Tokens（变量为用户ID）
+     */
+    public static final String TOKENS = "tokens:%d";
+
+    /**
+     * 用户ID（变量为token）
+     */
+    public static final String USER_ID = "user-id:%s";
+
+    /**
+     * 验证码（变量为获取验证码时的临时字符串）
+     */
+    public static final String CAPTCHA = "captcha:%s";
+
+    /**
+     * 用户（变量为用户ID）
+     */
+    public static final String USER = "user:%d";
+
+    /**
+     * 角色集合（变量为用户ID）
+     */
+    public static final String ROLES = "roles:%d";
+
+    /**
+     * 权限集合（变量为用户ID）
+     */
+    public static final String PERMISSIONS = "permissions:%d";
 
 }
