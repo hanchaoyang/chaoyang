4bcf56c4f950599e24193b0a84e68ad321efdca5
韩朝阳 <468455321@qq.com>
2023-05-31 11:07:22 +0800

    feat: 优化变量命名

--- chaoyang/src/main/java/com/chaoyang/example/entity/dto/request/LoginRequest.java
+++ chaoyang/src/main/java/com/chaoyang/example/entity/dto/request/LoginRequest.java
@@ -15,31 +15,31 @@ import javax.validation.constraints.NotBlank;
 public class LoginRequest {
 
     /**
-     * 用户手机号
+     * 用户账号
      */
-    @NotBlank(message = "用户手机号不能为空")
-    @Length(min = 11, max = 11, message = "用户手机号长度为11个字符")
-    private String userPhone;
+    @NotBlank(message = "用户账号不能为空")
+    @Length(min = 4, max = 20, message = "用户账号长度为4-20个字符")
+    private String account;
 
     /**
      * 用户密码
      */
     @NotBlank(message = "用户密码不能为空")
     @Length(min = 6, max = 20, message = "用户密码长度为6-20个字符")
-    private String userPassword;
+    private String password;
 
     /**
      * 临时字符串
      */
-//    @NotBlank(message = "临时字符串不能为空")
-//    @Length(min = 4, max = 32, message = "临时字符串长度为4-32个字符")
+    @NotBlank(message = "临时字符串不能为空")
+    @Length(min = 4, max = 32, message = "临时字符串长度为4-32个字符")
     private String nonce;
 
     /**
      * 验证码
      */
-//    @NotBlank(message = "验证码不能为空")
-//    @Length(min = 4, max = 4, message = "验证码长度为4个字符")
+    @NotBlank(message = "验证码不能为空")
+    @Length(min = 1, max = 4, message = "验证码长度为1-4个字符")
     private String captcha;
 
 }
