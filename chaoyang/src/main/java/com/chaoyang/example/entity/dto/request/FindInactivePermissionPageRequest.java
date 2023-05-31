1b498ff03d3499218793e4c39cf6a1be1099bd2b
韩朝阳 <468455321@qq.com>
2023-05-31 10:56:00 +0800

    feat: 优化变量命名

--- chaoyang/src/main/java/com/chaoyang/example/entity/dto/request/FindInactivePermissionPageRequest.java
+++ chaoyang/src/main/java/com/chaoyang/example/entity/dto/request/FindInactivePermissionPageRequest.java
@@ -6,7 +6,7 @@ import org.hibernate.validator.constraints.Range;
 import javax.validation.constraints.NotNull;
 
 /**
- * 分页查询未使用的权限请求类
+ * 分页查询角色未关联的权限请求类
  *
  * @author 韩朝阳
  * @since 2023/3/23
@@ -15,22 +15,23 @@ import javax.validation.constraints.NotNull;
 public class FindInactivePermissionPageRequest {
 
     /**
-     * 角色主键
+     * 角色ID
      */
-    @NotNull(message = "角色主键不能为空")
+    @NotNull(message = "角色ID不能为空")
     private Long roleId;
 
     /**
      * 页码
      */
     @NotNull(message = "页码不能为空")
+    @Range(min = 1L, max = 100000000L, message = "页码为1-100000000")
     private Long current;
 
     /**
      * 页面大小
      */
     @NotNull(message = "页面大小不能为空")
-    @Range(min = 1L, max = 100L, message = "页面大小为1-100")
+    @Range(min = 1L, max = 1000L, message = "页面大小为1-10000")
     private Long size;
 
 }
