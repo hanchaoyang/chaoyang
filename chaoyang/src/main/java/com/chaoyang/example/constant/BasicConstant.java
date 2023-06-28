63327e4c576a7d7cd38f25a2d70ad7a39fc64cc3
韩朝阳 <468455321@qq.com>
2023-06-28 18:03:57 +0800

    feat: 优化代码

--- chaoyang/src/main/java/com/chaoyang/example/constant/BasicConstant.java
+++ chaoyang/src/main/java/com/chaoyang/example/constant/BasicConstant.java
@@ -1,7 +1,7 @@
 package com.chaoyang.example.constant;
 
 /**
- * 是否是基础数据常量类（基础数据不能被删除，如超管账号、超管角色、基础权限等）
+ * 是否是基础数据常量类（基础数据不能被删除，如管理员账号、管理员角色、基础权限等）
  *
  * @author 韩朝阳
  * @since 2023/5/22
@@ -14,7 +14,7 @@ public class BasicConstant {
     public static final Integer FALSE = 0;
 
     /**
-     * 否
+     * 是
      */
     public static final Integer TRUE = 1;
 
