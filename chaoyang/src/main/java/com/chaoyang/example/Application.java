20b46de6367ac1bb5e8af6e000d3d54b3a05bef2
韩朝阳 <468455321@qq.com>
2023-06-28 17:40:41 +0800

    feat: 优化代码

--- chaoyang/src/main/java/com/chaoyang/example/Application.java
+++ chaoyang/src/main/java/com/chaoyang/example/Application.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        try {
+            SpringApplication.run(Application.class, args);
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
     }
 
 }
